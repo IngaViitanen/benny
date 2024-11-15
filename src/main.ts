c7f37541f45660799566a9df53ff4f6dd38f0799
Inga Viitanen <ingaa134@gmail.com>
2024-11-15 18:43:56 +0100

    vite and mobile or desktop

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import './style.css'
+const app = document.querySelector<HTMLElement>("#app")
+let gameSrc = ''
+
+const checkDeviceType = () => {
+  console.log(window.innerWidth)
+   if(window.innerWidth < 1024){
+    return gameSrc = "/mobileGame/BennyStarfighter_mobile.html"
+   } else {
+    return gameSrc = "/game/BennyStarfighter.html"
+   }
+}
+
+checkDeviceType()
+
+app!.innerHTML = `
+  <iframe id="iframe" src=${gameSrc} frameborder="0"></iframe>
+`
+
+// <iframe id="iframe" src="/mobileGame/BennyStarfighter_mobile.html" frameborder="0"></iframe>
